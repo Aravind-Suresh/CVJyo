4fe2caddaa46e1cb122a7c3d307f35fb6dade954
aravind <arvindsuresh2009@gmail.com>
2015-03-06 18:16:04 +0530

    implemented watershed in c++

--- watershed1.cpp
+++ watershed1.cpp
@@ -7,20 +7,19 @@ using namespace std;
 int value=0;
 
 Mat img_gray_d_t_thresh_G;
+Mat img_gray_d_t_G;
 
 void dt_thresh_callback(int , void*) {
-	threshold(img_gray_d_t_thresh_G, img_gray_d_t_thresh_G, value, 255, THRESH_BINARY);
-	namedWindow("dt-thresh", WINDOW_AUTOSIZE);
+	cout<<value<<endl;
+	threshold(img_gray_d_t_G, img_gray_d_t_thresh_G, value, 255, THRESH_BINARY);
 	imshow("dt-thresh", img_gray_d_t_thresh_G);	
 }
 
 int main(int argc, char** argv) {
 	Mat img_gray = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-	Mat img_gray_thresh(img_gray.rows, img_gray.cols, CV_8UC1, Scalar::all(0));
-	Mat img_gray_d_t_thresh(img_gray.rows, img_gray.cols, CV_8UC1, Scalar::all(0));
-	Mat img_gray_d_t(img_gray.rows, img_gray.cols, CV_8UC1, Scalar::all(0));
-
-	img_gray_d_t_thresh.copyTo(img_gray_d_t_thresh_G);
+	Mat img_gray_thresh(img_gray.rows, img_gray.cols, CV_32F, Scalar::all(0));
+	Mat img_gray_d_t_thresh(img_gray.rows, img_gray.cols, CV_32F, Scalar::all(0));
+	Mat img_gray_d_t(img_gray.rows, img_gray.cols, CV_32F, Scalar::all(0));
 
 	threshold(img_gray, img_gray_thresh, 0, 255, THRESH_BINARY + THRESH_OTSU);
 	bitwise_not(img_gray_thresh, img_gray_thresh);
@@ -32,10 +31,12 @@ int main(int argc, char** argv) {
 	namedWindow("dt", WINDOW_AUTOSIZE);
 	imshow("dt", img_gray_d_t);
 
+	img_gray_d_t.copyTo(img_gray_d_t_G);
+
+	threshold(img_gray_d_t_G, img_gray_d_t_thresh_G, 0.5, 1, THRESH_BINARY);
 	namedWindow("dt-thresh", WINDOW_AUTOSIZE);
 	imshow("dt-thresh", img_gray_d_t_thresh_G);
-	threshold(img_gray_d_t_thresh_G, img_gray_d_t_thresh_G, value, 255, THRESH_BINARY);
-	createTrackbar("Value","dt-thresh",&value,255,dt_thresh_callback);
+	//createTrackbar("Threshold value","dt-thresh",&value,255,dt_thresh_callback);
 
 	waitKey(0);
 
